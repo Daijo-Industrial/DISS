a4c245e00b7b679aa2bfe350cb7ebbe2851d8208
Raymond Lay <raymondlay023@gmail.com>
2026-09-01 15:00:35 +0700

    fix port

--- resources/js/app.js
+++ resources/js/app.js
@@ -35,13 +35,17 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+const defaultWsPort = window.location.port
+  ? parseInt(window.location.port)
+  : (window.location.protocol === 'https:' ? 443 : 80);
+
 window.Echo = new Echo({
   broadcaster: 'reverb',
   key: import.meta.env.VITE_REVERB_APP_KEY ?? import.meta.env.VITE_PUSHER_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST ?? import.meta.env.VITE_PUSHER_HOST ?? window.location.hostname,
-  wsPort: import.meta.env.VITE_REVERB_PORT ?? import.meta.env.VITE_PUSHER_PORT ?? 80,
-  wssPort: import.meta.env.VITE_REVERB_PORT ?? import.meta.env.VITE_PUSHER_PORT ?? 443,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+  wsHost: import.meta.env.VITE_REVERB_HOST || import.meta.env.VITE_PUSHER_HOST || window.location.hostname,
+  wsPort: import.meta.env.VITE_REVERB_PORT ? parseInt(import.meta.env.VITE_REVERB_PORT) : defaultWsPort,
+  wssPort: import.meta.env.VITE_REVERB_PORT ? parseInt(import.meta.env.VITE_REVERB_PORT) : defaultWsPort,
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? import.meta.env.VITE_PUSHER_SCHEME ?? window.location.protocol.replace(':', '')) === 'https',
   enabledTransports: ['ws', 'wss'],
   disabledStats: true,
 });
