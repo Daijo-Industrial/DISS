fc8dc7428d2f9f846934995eed67c8075bafe2e4
Raymond Lay <raymondlay023@gmail.com>
2026-09-16 15:45:08 +0700

    fix: purchase order index invoice test

--- tests/Feature/PurchaseOrderIndexInvoicingSumsTest.php
+++ tests/Feature/PurchaseOrderIndexInvoicingSumsTest.php
@@ -25,11 +25,15 @@ class PurchaseOrderIndexInvoicingSumsTest extends TestCase
 
     public function test_it_calculates_filtered_invoicing_totals_for_partially_invoiced_pos()
     {
+        $category = \App\Models\PurchaseOrderCategory::create(['name' => 'General']);
+
         // 1. Partially invoiced PO: Total 1,000,000 IDR, Invoiced 400,000 IDR (Remaining 600,000 IDR)
         $po1 = PurchaseOrder::create([
             'po_number' => 1001,
             'creator_id' => $this->user->id,
+            'purchase_order_category_id' => $category->id,
             'vendor_name' => 'Vendor A',
+            'filename' => 'test1.pdf',
             'currency' => 'IDR',
             'total' => 1000000,
         ]);
@@ -45,7 +49,9 @@ class PurchaseOrderIndexInvoicingSumsTest extends TestCase
         $po2 = PurchaseOrder::create([
             'po_number' => 1002,
             'creator_id' => $this->user->id,
+            'purchase_order_category_id' => $category->id,
             'vendor_name' => 'Vendor B',
+            'filename' => 'test2.pdf',
             'currency' => 'IDR',
             'total' => 500000,
         ]);
@@ -61,7 +67,9 @@ class PurchaseOrderIndexInvoicingSumsTest extends TestCase
         $po3 = PurchaseOrder::create([
             'po_number' => 1003,
             'creator_id' => $this->user->id,
+            'purchase_order_category_id' => $category->id,
             'vendor_name' => 'Vendor C',
+            'filename' => 'test3.pdf',
             'currency' => 'IDR',
             'total' => 300000,
         ]);
@@ -93,5 +101,48 @@ class PurchaseOrderIndexInvoicingSumsTest extends TestCase
             ->call('filterByStat', 'partially_invoiced');
 
         $component->assertSet('invoicingFilter', 'partially_invoiced');
+    }
+
+    public function test_it_handles_multi_currency_and_ignores_soft_deleted_invoices()
+    {
+        $category = \App\Models\PurchaseOrderCategory::create(['name' => 'General']);
+
+        // USD PO: Total $2,000, Invoiced $500, soft-deleted invoice of $800
+        $poUsd = PurchaseOrder::create([
+            'po_number' => 2001,
+            'creator_id' => $this->user->id,
+            'purchase_order_category_id' => $category->id,
+            'vendor_name' => 'US Vendor',
+            'filename' => 'usd.pdf',
+            'currency' => 'USD',
+            'total' => 2000,
+        ]);
+        Invoice::create([
+            'purchase_order_id' => $poUsd->id,
+            'invoice_number' => 'INV-USD-1',
+            'invoice_date' => now(),
+            'total' => 500,
+            'total_currency' => 'USD',
+        ]);
+        $deletedInv = Invoice::create([
+            'purchase_order_id' => $poUsd->id,
+            'invoice_number' => 'INV-USD-DEL',
+            'invoice_date' => now(),
+            'total' => 800,
+            'total_currency' => 'USD',
+        ]);
+        $deletedInv->delete();
+
+        $component = Livewire::test(PurchaseOrderIndex::class)
+            ->set('invoicingFilter', 'partially_invoiced');
+
+        $invoicingTotals = $component->get('filteredInvoicingTotals');
+
+        $this->assertArrayHasKey('USD', $invoicingTotals);
+        $this->assertEquals(1, $invoicingTotals['USD']['po_count']);
+        $this->assertEquals(2000.0, $invoicingTotals['USD']['total_valuation']);
+        $this->assertEquals(500.0, $invoicingTotals['USD']['total_invoiced']);
+        $this->assertEquals(1500.0, $invoicingTotals['USD']['total_remaining']);
+        $this->assertEquals(25.0, $invoicingTotals['USD']['percent']);
     }
 }
